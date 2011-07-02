e4392f10f9e3869632ec5f150589562208f5b8ee
Dmitry Goncharov <dgoncharov@users.sf.net>
2011-07-02 19:44:03 -0400

    ref

--- perm/perm.c
+++ perm/perm.c
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
     if (*argv[0])
     {
         printf("%s\n", argv[1]);
-        argv[0][0] = '\0';
+        *argv[0] = '\0';
     }
     if (*(argv[1] + argc - 1))
         main(argc + 1, argv);
