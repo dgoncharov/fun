37a9405927cf06fb9dd75c147d075be18ab60d86
Dmitry Goncharov <dgoncharov@users.sf.net>
2011-07-02 12:29:00 -0400

    ref

--- perm.cpp
+++ perm.cpp
@@ -3,23 +3,34 @@
 #include <stddef.h>
 #include <string.h>
 
-void perm(size_t index, char* s)
+//void perm(size_t index, char* s)
+//{
+//    const size_t slen = strlen(s);
+//    if (index + 1 < slen)
+//        perm(index + 1, s);
+//    for (size_t k = index + 1; k < slen; ++k)
+//    {
+//        std::swap(s[index], s[k]);
+//        std::cout << s << std::endl;
+//        perm(index + 1, s);
+//        std::swap(s[index], s[k]);
+//    }
+//}
+
+int main(int argc, char* argv[])
 {
+    int index = argc - 2;
+    char* s = argv[1];
     const size_t slen = strlen(s);
     if (index + 1 < slen)
-        perm(index + 1, s);
+        main(index + 3, argv);
     for (size_t k = index + 1; k < slen; ++k)
     {
         std::swap(s[index], s[k]);
         std::cout << s << std::endl;
-        perm(index + 1, s);
+        main(index + 3, argv);
         std::swap(s[index], s[k]);
     }
-}
-
-int main(int argc, char* argv[])
-{
-    std::cout << argv[1] << std::endl;
-    perm(0, argv[1]);
+    
 }
 
