0e9baea06ab3c5d626a4ad726e53cf7308c7152b
Dmitry Goncharov <dgoncharov@users.sf.net>
2011-07-02 19:44:10 -0400

    added a test

---
+++ perm/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cc -o perm perm.c
+[[ 0 == $? ]] || exit 1
+n=$(./perm 12345 | sort -u |wc -l)
+if [[ 120 != $n ]]; then
+    echo failure $n != 120
+    exit 1
+fi
+n=$(./perm 1234567 | sort -u |wc -l)
+if [[ 5040 != $n ]]; then
+    echo failure $n != 5040
+    exit 1
+fi
+exit 0
+
