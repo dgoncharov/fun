caf8a1829cec2e2d85176de70d336d95f2fd1df1
Dmitry Goncharov <dgoncharov@users.sf.net>
2011-07-02 12:57:52 -0400

    in progress

--- perm.cpp
+++ perm.cpp
@@ -1,36 +1,30 @@
-#include <iostream>
-#include <utility>
-#include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
-//void perm(size_t index, char* s)
-//{
-//    const size_t slen = strlen(s);
-//    if (index + 1 < slen)
-//        perm(index + 1, s);
-//    for (size_t k = index + 1; k < slen; ++k)
-//    {
-//        std::swap(s[index], s[k]);
-//        std::cout << s << std::endl;
-//        perm(index + 1, s);
-//        std::swap(s[index], s[k]);
-//    }
-//}
+void swap(char* s1, char* s2)
+{
+    *s1 ^= *s2;
+    *s2 = *s1 ^ *s2;
+    *s1 ^= *s2;
+}
 
 int main(int argc, char* argv[])
 {
-    int index = argc - 2;
+    if (*argv[0]) {
+        printf("%s\n", argv[1]);
+        argv[0][0] = '\0';
+    }
+    const size_t index = argc - 2;
     char* s = argv[1];
     const size_t slen = strlen(s);
     if (index + 1 < slen)
         main(index + 3, argv);
     for (size_t k = index + 1; k < slen; ++k)
     {
-        std::swap(s[index], s[k]);
-        std::cout << s << std::endl;
+        swap(s + index, s + k);
+        printf("%s\n", s);
         main(index + 3, argv);
-        std::swap(s[index], s[k]);
+        swap(s + index, s + k);
     }
-    
 }
 
