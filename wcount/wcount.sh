97eefb81a7311c6623df271c74328a60aed172c0
Dmitry Goncharov <dgoncharov@users.sf.net>
2011-12-11 22:50:33 -0500

    works with words which can be used as bash variables

--- wcount/wcount.sh
+++ wcount/wcount.sh
@@ -1,70 +1,52 @@
 #!/bin/bash
 
-#count[2]=1
-#count[3]=4
-#r=count
-#words[gore]=r
+# This program reads the specified file and for every word in the file prints
+# to stdout the number of occurences of that word in every line where the word
+# is present.
 #
-#ref=$(eval echo \$${words[gore]})
-#echo ${ref}
-#eval echo \${$ref[@]}
-#
-#
-#arr=(one two three)
-#ref=arr
-#tab[gore]=ref
-#a=$(eval echo \$${tab[gore]})
-#eval echo \${$a[*]}
-#
-#eval set \${$a[3]=four}
-#eval set \${$a[4]=five}
-#eval echo \${$a[@]}
+# Distributed under the terms of the bsd license.
+# Copyright (c) 2011 Dmtiry Goncharov (dgoncharov@users.sf.net).
 
-# {word1 => {linenum1 => noccurences1, linenum2 => noccurences2}, word2 =>
-# {linenum1 => noccurences3}}
+if [[ $# != 1 ]]
+then
+    echo "usage: $0 <filename>"
+    exit 1
+fi
 
-echo one one one one >> /tmp/1
-echo two >> /tmp/1
-echo two >> /tmp/1
-echo three >> /tmp/1
-echo three >> /tmp/1
-echo three >> /tmp/1
-
+readonly src=$1
 
 declare -A words
-declare -a lines
 
-linenum=1
+linenum=0
 while read line 
 do
     for w in $line
     do
-        cnt=${lines[$linenum]}
-        let cnt+=1
-        lines[$linenum]=$cnt
-        words[$w]=lines
-#        words[gore]=lines
+        l=${w}lines
+        c=${w}copies
+        declare -a ${l}
+        declare -a ${c}
+        let ${l}[$linenum]=$linenum
+        let ${c}[$linenum]+=1
+        words[$w]=a
     done
     let linenum+=1
-done < /tmp/1
+done < ${src}
 
-#testlines=(6 7 8)
-#words[gore]=testlines
-#r=${words[gore]}
-#for n in $(eval echo \${!$r[@]})
-#do
-#    echo -n '$words[gore]['$n']='
-#    eval echo \${$r[$n]}
-#done
-
-for k in ${!words[@]}
+for w in ${!words[@]}
 do
-#    echo '${words['$k']}='${words[$k]}
-    r=${words[$k]}
-    for n in $(eval echo \${!$r[@]})
+    echo $w
+    lns=${w}lines[@]
+    declare -a lines=("${!lns}")
+
+    cps=${w}copies[@]
+    declare -a copies=("${!cps}")
+
+    cnt=0
+    for i in ${lines[@]}
     do
-        echo -n '$words['$k']['$n']='
-        eval echo \${$r[$n]}
+        echo "    ${copies[$cnt]} occurences in line $i"
+        let cnt+=1
     done
 done
 
