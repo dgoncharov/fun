330dfddc74d1f9fa2bc65b77c4ec6505c57d00a8
Dmitry Goncharov <dgoncharov@users.sf.net>
2022-03-19 22:45:33 -0400

    Add intdef.c.

---
+++ intdef/intdef.c
@@ -0,0 +1,122 @@
+/* Various tests which demonstrate deficiencies of integer arithmetic in c.  */
+
+#include <locale.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+static
+void f1()
+{
+    int32_t x = 0x80808080;
+    uint32_t y = 0x80808080;
+    assert((uint32_t) x == y);
+    x >>= 1;
+    y >>= 1;
+    assert((uint32_t) x == y);
+}
+
+static
+void f2()
+{
+    int32_t x = 0x80808080;
+    uint32_t y = 0x80808080;
+    assert((uint32_t) x == y);
+    int shift = 32;
+    x >>= shift;
+    y >>= shift;
+    assert((uint32_t) x == y);
+}
+
+static
+void f3()
+{
+    int32_t x = 0x80808080;
+    uint32_t y = 0x80808080;
+    assert((uint32_t) x == y);
+    int shift = 32;
+    x <<= shift;
+    y <<= shift;
+    assert((uint32_t) x == y);
+}
+
+static
+void f4()
+{
+    assert(-1 < 0u);
+}
+
+static
+void f5()
+{
+    char* locale = setlocale(LC_ALL, "en_US.arr1");
+    assert(locale);
+    signed char c = 0xc0;
+    int x = isupper((signed char) c);
+    int y = isupper((unsigned char) c);
+    assert(x == y);
+}
+
+static
+void f6()
+{
+    signed char arr[256];
+    memset (arr, 'w', sizeof arr);
+
+    signed char index = 0xf7;
+    signed char x = arr[index];
+    signed char y = arr[0xf7];
+    assert (x == y);
+}
+
+static
+void f7()
+{
+    assert(2147483647u > -2147483647 - 1);
+}
+
+static
+void f8()
+{
+    int32_t x = 2147483648u;
+    int32_t y = 2147483647;
+    assert(y < x);
+}
+
+static
+void f9()
+{
+    int x = strlen("1") - strlen("11") > 0;
+    assert(x == 0);
+}
+
+static
+void safe_memcpy(char* dst, const char* src, int srclen, int nbytes)
+{
+    memcpy(dst, src, srclen < nbytes ? srclen : nbytes);
+}
+
+static
+void f10()
+{
+    char dst[64], src[64];
+    safe_memcpy(dst, src, 64, -1);
+}
+
+int main()
+{
+    f10();
+    f9();
+    f8();
+    f7();
+    f6();
+    f5();
+    f4();
+    f3();
+    f2();
+    f1();
+    return 0;
+}
+
+
